503c387a1acb65023ab4a9f93142acdb272fcdbb
sodizdaroglu <americanlaz@gmail.com>
2020-11-10 00:26:50 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  3 - Write a program and ask the user to enter 5 numbers.If a number has been previously entered, 
+            //      display an error message and ask the user to re-try.
+            //       Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+            var numbers = new List<int>();
+
+            while (numbers.Count < 5)
+            {
+                Console.Write("Enter a number: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine("You've previously entered " + number);
+                    continue;
+                }
+
+                numbers.Add(number);
+            }
+
+            numbers.Sort();
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+        
+
+
+
+
+
+
+
+        // Console.WriteLine("xxxxxx");
+
+
+
+
+
+
+
+
+        //==========================================
+
+
+
+
+
+
+    }
+}       
+}
